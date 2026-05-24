21e5bd9125facce85e270cf450393913141b693e
KimLong0910-pro <tranlong.9.1236@gmail.com>
2026-05-24 18:06:59 +0700

    first commit

---
+++ bài 1.1.py
@@ -0,0 +1,13 @@
+def tuyen_tinh(array, n, x):
+
+    for i in range(0, n):
+        if (array[i]==x):
+            return i
+        else:
+            i += 1
+
+array = [20, 30, 15, 5, 10, 40]
+x = 40
+n = len(array)
+result = tuyen_tinh(array, n, x)
+print(f"Phan tu tim thay duoc tai vi tri la: {result}")
---
+++ bài 1.2.py
@@ -0,0 +1,31 @@
+def tuyen_tinh(array, n, x):
+    for i in range(0, n):
+            if (array[i] == x):
+                return i
+            else:
+                i += 1
+                
+                
+
+array = [15, 25, 80, 30, 60, 50, 110, 100, 130, 180]
+x  = 110
+n = len(array)
+kqua = tuyen_tinh(array, n, x)
+print(f'phan tu tim thay duoc tai vi tri la: {kqua}')
+
+
+def tuyen_tinh2(array2, n2, x2):
+    for i in range(0, n2):
+        if (array2[i] == x2):
+            return i
+        else: 
+            return -1
+            
+        
+array2 = [15, 25, 80, 30, 60, 50, 110, 100, 130, 180]
+x2 = 25
+n2 = len(array2)
+kqua2 = tuyen_tinh2(array2, n2, x2)
+print(f'phan tu tim thay duoc tai vi tri la: {kqua2}')
+
+
