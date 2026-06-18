5f3c9d42999ddcceb42385523a74c9f80b92429e
KimLong0910-pro <tranlong.9.1236@gmail.com>
2026-06-19 00:01:36 +0700

    update

---
+++ Lab-05/Bài tập vui vẻ/bài 03.py
@@ -0,0 +1,36 @@
+def dijkstra(graph, start):
+    n = len(graph)
+    dist = [float("inf")] * n
+    visited = [False] * n
+    dist[start] = 0
+
+    for _ in range(n):
+        u = -1
+
+        for i in range(n):
+            if not visited[i] and (u == -1 or dist[i] < dist[u]):
+                u = i
+
+        if dist[u] == float("inf"):
+            break
+
+        visited[u] = True
+
+        for v, w in graph[u]:
+            if not visited[v] and dist[u] + w < dist[v]:
+                dist[v] = dist[u] + w
+
+    return dist
+
+
+graph = [
+    [(1, 3), (2, 1)],
+    [(0, 3), (2, 2), (3, 4)],
+    [(0, 1), (1, 2), (3, 3)],
+    [(1, 4), (2, 3), (4, 3)],
+    [(3, 3), (5, 2)],
+    [(4, 2)],
+]
+
+dist = dijkstra(graph, 0)
+print(dist)
