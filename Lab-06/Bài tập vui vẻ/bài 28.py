38371b0b031fd3a500db6710f3b12d5b9cdaa939
KimLong0910-pro <tranlong.9.1236@gmail.com>
2026-07-01 18:48:12 +0700

    update

--- Lab-06/Bài tập vui vẻ/bài 28.py
+++ Lab-06/Bài tập vui vẻ/bài 28.py
@@ -15,3 +15,13 @@ class QueueByStack:
             return None
 
         return self.out_stack.pop()
+    
+q = QueueByStack()
+
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+print(q.dequeue())
+print(q.dequeue())
+print(q.dequeue())
